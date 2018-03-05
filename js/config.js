f5d5778d529ccdab790bfe3dfdef8c667cf280fa
NguyenHong06 <hongnguyen060895@gmail.com>
2018-03-05 11:50:26 +0700

    xóa config

--- js/config.js
+++ js/config.js
@@ -3,38 +3,6 @@ define([
 ], function () {
     'use strict';
     return {
-        basemap: {
-            title: 'Dữ liệu nền Bình Dương',
-            id: 'dulieunen',
-            url: 'https://ditagis.com:6443/arcgis/rest/services/BinhDuong/DuLieuNen/MapServer',
-            // visible: false,
-            copyright: 'Bản đồ biên tập bởi Trung tâm DITAGIS',
-            layerInfos: [
-                {
-                    id: 5,
-                    title: 'Hành chính huyện'
-                },
-                {
-                    id: 4,
-                    title: 'Hành chính xã'
-                }, {
-                    id: 3,
-                    title: 'Phủ bề mặt',
-                    visible: false
-                },
-                {
-                    id: 2,
-                    title: 'Mặt giao thông',
-                    visible: false
-                }, {
-                    id: 1,
-                    title: 'Sông hồ'
-                }, {
-                    id: 0,
-                    title: 'Tim đường'
-                }
-            ]
-        },
         layers: [{
             title: 'Sự cố',
             id: "SuCo",
